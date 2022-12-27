8ebf7534b639da4ba3382ad27919120d5e13efa5
Frensi Angjo <67911608+Fre-an@users.noreply.github.com>
2022-12-27 18:49:20 -0500

    Trivia

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "output": {
+    "filename": "[name].pack.js"
+  },
+  "module": {
+    "rules": [
+      {
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "plugins":['transform-object-rest-spread'],
+            "presets": [
+              "babel-preset-env",
+              "babel-preset-react"
+            ]
+          }
+        },
+        "exclude": /node_modules/,
+        "test": /\.js$/
+      }
+    ]
+  },
+  "entry": {
+    "index": "./index"
+  }
+};
